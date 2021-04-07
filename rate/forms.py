ad1cca97e0d87925bbc37af59acdacfb99c0195b
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-07 12:33:31 +0300

    create project upload form

--- rate/forms.py
+++ rate/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import *
+from .models import Posts, Profile
 
 class UpdateProfile(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['bio', 'profile_pic']
         exclude = ['user']
+
+class UploadProjects(forms.ModelForm):
+    class Meta:
+        model = Posts
+        fields = ['project_name', 'descrition', 'project_link', 'project_image']
+        exclude = ['user_prof']
