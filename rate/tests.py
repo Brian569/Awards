bf83ca7b5dee6ca92c5784b3a429f455cef5c485
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-07 12:35:52 +0300

    run profile tests

--- rate/tests.py
+++ rate/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class ProfileTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='bmn')
+        self.profile = Profile.objects.create(user=self.user, bio='nicely done')
+
+    def instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def save_prof(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def get_profile(self):
+        self.profile.save()
+        profile = Profile.get_profile()
+        self.assertTrue(len(profile) > 0)
