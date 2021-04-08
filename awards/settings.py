0eea656af05ccd17c0115a6362a28b6e12e9b145
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-04-08 12:20:43 +0300

    cloud name

--- awards/settings.py
+++ awards/settings.py
@@ -132,7 +132,7 @@ LOGIN_REDIRECT_URL = '/'
 
 
 cloudinary.config( 
-  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'), 
+  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME', 'dsxfbf0gr'), 
   api_key = os.environ.get('API_KEY'), 
   api_secret = os.environ.get('API_SECRET') 
 )
